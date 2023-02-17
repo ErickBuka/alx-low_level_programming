cf6c5efb05d3171d548d62e7fe9c0d9cd31ca9bf
ErickBuka <112546640+ErickBuka@users.noreply.github.com>
2023-02-17 14:58:34 -0800

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,12 +8,19 @@ int main(void)
 {
 	char c;
 
+	char d;
+
 	c = 'a';
+	d = 'A';
 	while
 		(c <= 'z') {
-			if ((c != 'q' && c != 'e') && c <= 'z')
-				putchar(c);
+			putchar(c);
 			c++;
+		}
+	while
+		(d <= 'Z') {
+			putchar (d);
+			d++;
 		}
 	putchar('\n');
 	return (0);
