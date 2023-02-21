c4b289d46a9462c80964b512be42ba90352aa858
ErickBuka <112546640+ErickBuka@users.noreply.github.com>
2023-02-21 12:23:19 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * print_alphabet_x10 - a function that prints 10 times the alphabet
+ *
+ * Return: x10 a-z
+ */
+void print_alphabet_x10(void)
+{
+	int n, m;
+
+	m = 0;
+
+	while (m < 10)
+	{
+		for (n = 'a'; n <= 'z'; n++)
+		{
+			_putchar(n);
+		}
+		m++;
+		_putchar('\n');
+	}
+}
