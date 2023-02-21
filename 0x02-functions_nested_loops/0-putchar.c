b25e4fe45267c4ff7ba219ab2229c8a0f74280d0
ErickBuka <112546640+ErickBuka@users.noreply.github.com>
2023-02-21 11:19:35 -0800

    0._putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include "stdio.h"
+#include "unistd.h"
+
+/**
+ * main - Program that prints _putchar
+ * followed by a new line
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,4 @@
+1. program that prints _putchar, followed by a new line.
+2. Write a function that prints the alphabet, in lowercase, followed by a new line.
+3. Write a function that prints 10 times the alphabet, in lowercase, followed by a new line.
+
