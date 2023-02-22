3772299fd36a1d79405e03103c9e75928c1f3a08
ErickBuka <112546640+ErickBuka@users.noreply.github.com>
2023-02-21 16:07:11 -0800

    102-fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - print the first 50 fibonacci numbers.
+ * Return: Nothing.
+ */
+int main(void)
+{
+	unsigned long count, a, b, c;
+
+	a = 0;
+	b = 1;
+
+	for (count = 0; count < 50; count++)
+	{
+		c = a + b;
+		a = b;
+		b = c;
+		printf("%lu", c);
+		if (count == 49)
+			putchar('\n');
+		else
+		{
+			printf(", ");
+		}
+	}
+	return (0);
+}
