815093ab950521785752bab1d9df5b333892eeec
ErickBuka <112546640+ErickBuka@users.noreply.github.com>
2023-02-24 11:30:33 -0800

    function that prints a square, followed by a new line

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -3,24 +3,23 @@
 /**
  * print_square - prints a square, followed by a new line;
  * @size: size of the square
- * Return: void
  */
-
-void print_square(int n)
+void print_square(int size)
 {
-	int i = 0, ii;
-
-	while (i < n && n > 0)
-	{
-		ii = 0;
-		while (ii < n)
-		{
-			_putchar('#');
-			ii++;
-		}
-		_putchar('\n');
-		i++;
-	}
-	if (i == 0)
-		_putchar('\n');
+int i, j;
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
 }
