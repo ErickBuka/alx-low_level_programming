f42a229a71a86a2ccffab10e50c9de596c64c8a1
ErickBuka <112546640+ErickBuka@users.noreply.github.com>
2023-03-14 14:40:43 -0700

    correction on betty style

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,13 +17,13 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL)
 		s1 = "";
-	if (s2 == NULL)if (s2 == NULL)
+	if (s2 == NULL)
+	if (s2 == NULL)
 		s2 = "";
 
 	for (len1 = 0; s1[len1] != '\0'; len1++)
 		;
-	for (len2 = 0; s2[len2] != '\0'; len2++)
-		;
+	for (len2 = 0; s2[len2] != '\0'; len2++);
 
 	len = len1 + len2;
 	conStr = malloc(sizeof(char) * (len + 1));
@@ -36,7 +36,7 @@ char *str_concat(char *s1, char *s2)
 	for (i = 0; i < len1; i++)
 		conStr[i] = s1[i];
 
-       	j = len2;
+		j = len2;
 	for (len2 = 0; len2 <= j; i++, len2++)
 		conStr[i] = s2[len2];
 	return (conStr);
