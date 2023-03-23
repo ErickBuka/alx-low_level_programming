04600ca40dc10fcf99d4b977cdcb29609ac39e51
ErickBuka <112546640+ErickBuka@users.noreply.github.com>
2023-03-22 23:51:40 -0700

    rogram that prints the opcodes of its own main function.

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -9,33 +9,28 @@
  *
  * Return: Always 0 (Success)
  */
-
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	ud_t ud_obj;
-	int val = 0, i = 0;
-
-	if (argc == 2)
+	if (argc != 2)
 	{
-		val = atoi(argv[1]);
-
-		if (val < 0)
-		{
-			printf("Error\n");
-			exit(2);
-		}
-
-		ud_unit(&ud_obj);
-		ud_set_input_buffer(&ud_obj, argv[1], val);
-		ud_set_mode(&ud_obj, 64);
-		ud_set_syntax(&ud_obj, UD_SYN_INTEL);
-
-		while (ud_disassemble(&ud_obj))
-		{
-			printf("\t%s\n", ud_insn_hex(&ud_obj));
-		}
+		printf("Error\n");
+		return 1;
 	}
 
-	return (0);
-}
+	int num_bytes = atoi(argv[1]);
+	if (num_bytes < 0)
+	{
+		printf("Error\n");
+		return 2;
+	}
 
+	/*print the opcodes */
+	unsigned char *ptr = (unsigned char *)main;
+	for (int i = 0; i < num_bytes; i++)
+	{
+		printf("%02x", *(ptr + i));
+	}
+	printf("\n");
+
+	return 0;
+}
