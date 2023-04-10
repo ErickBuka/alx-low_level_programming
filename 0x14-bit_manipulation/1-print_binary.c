1e434092e15de6271eb5d5edfd24dc37510c176b
ErickBuka <112546640+ErickBuka@users.noreply.github.com>
2023-04-10 06:13:27 -0700

    print binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+
+* print_binary - Prints the binary representation of a number
+
+* @n: Number to be printed in binary
+
+* Return: void
+*/
+void print_binary(unsigned long int n)
+{
+	unsigned long int mask = 1;
+
+/* Find the most significant bit */
+	while (mask <= n >> 1)
+	{
+		mask <<= 1;
+	}
+
+/* Print the binary representation */
+	while (mask > 0)
+	{
+/* Check if the bit at mask position is set */
+	if (n & mask)
+	{
+		_putchar('1');
+	}
+	else
+	{
+		_putchar('0');
+	}
+	mask >>= 1;
+	/* Shift mask to the right by 1 bit */
+	}
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,15 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
 #include <limits.h>
 #include <stdio.h>
 
-unsigned int binary_to_uint(const char *b);
 int _putchar(char c);
+unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 void _right_pbin_mask(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
